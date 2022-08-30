929149fdfa1c58a67b850f42a97637b615934858
Dmitriy Rykov <dimon.man.18@gmail.com>
2022-08-30 16:45:05 +0300

    add test_guest_cant_see_product_in_basket_opened_from_main_page in test main page

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -1,9 +1,9 @@
 from .base_page import BasePage
-from .locators import BasketPage
+from .locators import BasketPageLocators
 
 class BasketPage(BasePage):
     def no_products_in_the_basket(self):
-        assert self.is_not_element_present(*BasketPage.PRODUCT_IN_BASKET), "products in the basket, should not be"
+        assert self.is_not_element_present(*BasketPageLocators.PRODUCT_IN_BASKET), "products in the basket, should not be"
 
     def message_that_the_basket_is_empty(self):
-        assert self.is_element_present(*BasketPage.MESSAGE_BASKET_IS_EMPTY), "there is no message that the basket is empty"
+        assert self.is_element_present(*BasketPageLocators.MESSAGE_BASKET_IS_EMPTY), "there is no message that the basket is empty"
--- pages/locators.py
+++ pages/locators.py
@@ -20,6 +20,6 @@ class BasePageLocators():
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     BASKET_LINK = (By.CSS_SELECTOR, ".btn-group a.btn-default")
 
-class BasketPage():
+class BasketPageLocators():
     PRODUCT_IN_BASKET = (By.CSS_SELECTOR, ".basket_summary")
     MESSAGE_BASKET_IS_EMPTY = (By.CSS_SELECTOR, "#content_inner > p")
